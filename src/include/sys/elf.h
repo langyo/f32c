5ec6469b1d43b7f67dfb1a4102a3eb26576a49e2
Marko Zec <mzec@koncar-institut.hr>
2026-04-17 14:40:45 +0200

    src/lib: attempt to better guesstimate the actual memory footprint and exact section placement therein

--- src/include/sys/elf.h
+++ src/include/sys/elf.h
@@ -2,7 +2,7 @@
 #ifndef _SYS_ELF_H_
 #define _SYS_ELF_H_ 1
 
-int elfinfo(int, uint32_t *, uint32_t *, uint32_t *, uint32_t *);
+int elfinfo(int, uint32_t *, uint32_t *, uint32_t *);
 int elfload(int, char *, uint32_t, char *, uint32_t);
 
 #endif /* !_SYS_ELF_H_ */
--- src/lib/src/sys/elf.c
+++ src/lib/src/sys/elf.c
@@ -5,7 +5,7 @@
 
 
 int
-elfinfo(int fd, uint32_t *entry, uint32_t *tsiz, uint32_t *dsiz, uint32_t *rsiz)
+elfinfo(int fd, uint32_t *entry, uint32_t *tsiz, uint32_t *dsiz)
 {
 	Elf32_Ehdr ehdr;
 	Elf32_Shdr shdr;
@@ -41,29 +41,23 @@ elfinfo(int fd, uint32_t *entry, uint32_t *tsiz, uint32_t *dsiz, uint32_t *rsiz)
 	if (lseek(fd, ehdr.e_shoff, SEEK_SET) != ehdr.e_shoff)
 		return (-1);
 
-	/* Compute text, data and relocation table sizes */
+	/* Compute total text and data segment sizes */
 	*entry = ehdr.e_entry;
 	*tsiz = *dsiz = 0;
-	if (rsiz != NULL)
-		*rsiz = 0;
 	for (i = 0; i < ehdr.e_shnum; i++) {
 		if (read(fd, &shdr, sizeof(shdr)) != sizeof(shdr))
 			return (-1);
 		
 		/* Round up allocation to word-aligned size */
 		len = (shdr.sh_size + 3) & ~3;
-		if (shdr.sh_type == SHT_REL && rsiz != NULL) {
-			*rsiz += len;
-			continue;
-		}
 		if ((shdr.sh_flags & SHF_ALLOC) == 0)
 			continue;
 		if (shdr.sh_type != SHT_PROGBITS && shdr.sh_type != SHT_NOBITS)
 			continue;
 		if (shdr.sh_flags & SHF_EXECINSTR)
-			*tsiz += len;
+			*tsiz = shdr.sh_addr - *entry + len;
 		else
-			*dsiz += len;
+			*dsiz = shdr.sh_addr - *entry - *tsiz + len;
 	}
 
 	return (0);
@@ -75,7 +69,7 @@ elfload(int fd, char *tp, uint32_t tsiz, char *dp, uint32_t dsiz)
 {
 	Elf32_Ehdr ehdr;
 	Elf32_Shdr shdr;
-	uint32_t tpos, dpos;
+	uint32_t off;
 	int i, len;
 
 	if (lseek(fd, 0, SEEK_SET) != 0)
@@ -85,7 +79,7 @@ elfload(int fd, char *tp, uint32_t tsiz, char *dp, uint32_t dsiz)
 		return (-1);
 
 	/* Read text and data sections to memory */
-	for (i = 0, tpos = 0, dpos = 0; i < ehdr.e_shnum; i++) {
+	for (i = 0; i < ehdr.e_shnum; i++) {
 		if (lseek(fd, ehdr.e_shoff + sizeof(shdr) * i, SEEK_SET) !=
 		    ehdr.e_shoff + sizeof(shdr) * i)
 			return (-1);
@@ -105,25 +99,21 @@ elfload(int fd, char *tp, uint32_t tsiz, char *dp, uint32_t dsiz)
 		if (lseek(fd, shdr.sh_offset, SEEK_SET) != shdr.sh_offset)
 			return (-1);
 
+		off = shdr.sh_addr - ehdr.e_entry;
 		if (shdr.sh_flags & SHF_EXECINSTR) {
-			if (tpos + len > tsiz)
+			if (off + len > tsiz)
 				return (-1);
-			if (read(fd, &tp[tpos / sizeof(*tp)], len) != len)
+			if (read(fd, &tp[off], len) != len)
 				return (-1);
-			tpos += len;
 		} else {
-			if (dpos + len > dsiz)
+			if (off - tsiz + len > dsiz)
 				return (-1);
 			if (shdr.sh_type == SHT_NOBITS)
-				memset(&dp[dpos / sizeof(*dp)], 0, len);
-			else if (read(fd, &dp[dpos / sizeof(*dp)], len) != len)
+				memset(&dp[off - tsiz], 0, len);
+			else if (read(fd, &dp[off - tsiz], len) != len)
 				return (-1);
-			dpos += len;
 		}
 	}
-
-	if (tpos != tsiz || dpos != dsiz)
-		return (-1);
 
 	return (0);
 }
